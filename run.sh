622438c004912082ce5d56fe5051a308d569c347
Nitish <24uec217@lnmiit.ac.in>
2025-08-02 14:43:25 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gnome-terminal -- bash -c "source ~/Desktop/vscode/myenv/bin/activate && uvicorn api.main:app --reload; exec bash"
-
-gnome-terminal -- bash -c "source ~/Desktop/vscode/myenv/bin/activate && streamlit run src/frontend.py; exec bash"
